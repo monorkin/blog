692ec242374841ad16d70aba7cd51f42765a9b74
Stanko K.R <me@stanko.io>
2021-07-10 18:09:31 +0200

    Don't backlink to search engines

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 module LinkHelper
+  SEARCH_ENGINES = Set.new(
+    %w[
+      www.google.com
+      www.bing.com
+      search.yahoo.com
+      www.baidu.com
+      duckduckgo.com
+      ask.com
+      www.wolframalpha.com
+      yandex.com
+      www.search.com
+    ]
+  ).freeze
+
   SEARCH_PROVIDERS = {
     duck_duck_go: 'https://duckduckgo.com/?q=%{term}'
   }.freeze
 
   def backlink(host, url, provider: :duck_duck_go, **options)
+    return host if SEARCH_ENGINES.include?(host)
+
     link_to(host,
             internet_search_url("#{url} site: #{host}", provider: provider),
             **options)

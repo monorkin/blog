75313fde960cf7b107a3bcde34005fb7eb95313d
Stanko K.R <stanko@stanko.io>
2022-07-01 11:35:24 +0200

    Use the same version of Tailwinf in foreman and asset compilation

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  darkMode: ['class', '.color-scheme.color-scheme--dark'],
-  content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}

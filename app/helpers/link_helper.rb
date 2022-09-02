dc082f6fa44284daf73b5974e6a4b81ad291e630
Stanko K.R <stanko@stanko.io>
2022-09-02 20:12:05 +0200

    Ignore more refferers from backlinks

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,33 +1,67 @@
 # frozen_string_literal: true
 
 module LinkHelper
-  SEARCH_ENGINES = Set.new(
-    %w[
-      www.google.com
-      www.bing.com
-      search.yahoo.com
-      www.baidu.com
-      duckduckgo.com
-      ask.com
-      www.wolframalpha.com
-      yandex.com
-      www.search.com
-    ]
-  ).freeze
+  IGNORED_HOSTS = [
+    'localhost',
+    /^(.+\.)?flipboard\..+$/,
+    /^com\.google\.android\..+$/,
+    'com.laurencedawson.reddit_sync',
+    'app.mailbrew.com',
+    /^(.+\.)?feedly\..+$/,
+    /^(.+\.)?inoreader\..+$/,
+    /^(.+\.)?stanko\.io$/
+  ].freeze
+
+  SEARCH_ENGINES = [
+    /^(.+\.)?google\..+$/,
+    /^(.+\.)?bing\..+$/,
+    /^(.+\.)?yahoo\..+$/,
+    /^(.+\.)?baiu\..+$/,
+    /^(.+\.)?duckduckgo\..+$/,
+    /^(.+\.)?wolframalpha\..+$/,
+    /^(.+\.)?yandex\..+$/,
+    /^(.+\.)?search\..+$/,
+    /^(.+\.)?startpage\..+$/
+  ].freeze
 
   SEARCH_PROVIDERS = {
     duck_duck_go: 'https://duckduckgo.com/?q=%{term}'
   }.freeze
 
-  def backlink(host, url, provider: :duck_duck_go, **options)
-    return host if SEARCH_ENGINES.include?(host)
+  HOST_MAPPINGS = {
+    't.co' => {
+      name: 't.co (Twitter)',
+      url: 'twitter.com'
+    },
+    'old.reddit.com' => {
+      name: 'old.reddit.com',
+      url: 'reddit.com'
+    },
+    'out.reddit.com' => {
+      name: 'out.reddit.com',
+      url: 'reddit.com'
+    }
+  }.freeze
 
-    link_to(host,
-            internet_search_url("#{url} site: #{host}", provider: provider),
+  def backlink(host, url, provider: :duck_duck_go, **options)
+    return host if ignored_host?(host) || search_engine?(host)
+
+    mapping = HOST_MAPPINGS.fetch(host, { name: host, url: host })
+
+    link_to(mapping[:name],
+            internet_search_url("#{url} site: #{mapping[:url]}", provider: provider),
             **options)
   end
 
   def internet_search_url(term, provider: :duck_duck_go)
     format(SEARCH_PROVIDERS[provider], term: CGI.escape(term))
+  end
+
+  def search_engine?(url)
+    SEARCH_ENGINES.any? { |engine| engine.match?(url) }
+  end
+
+  def ignored_host?(url)
+    IGNORED_HOSTS.any? { |host| host.match?(url) }
   end
 end

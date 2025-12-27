a94dd1ceb6b81bbd33782d552740993964f4dcfa
Stanko K.R. <stanko@stanko.io>
2025-12-27 18:52:13 +0100

    Switch to Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for tracing.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE', 0.1).to_f
+
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = ENV.fetch('SENTRY_PROFILES_SAMPLE_RATE', 0.1).to_f
+
+  # Filter sensitive data
+  config.send_default_pii = false
+
+  # Set the environment
+  config.environment = Rails.env
+
+  # Enable performance monitoring
+  config.enabled_environments = %w[production]
+end

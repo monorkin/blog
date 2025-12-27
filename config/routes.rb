84175e82b60f4dce846b02e4ac2ece6856ffb883
Stanko K.R. <stanko@stanko.io>
2025-12-27 18:43:30 +0100

    Replace dynamic error pages with static ones

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  get '401', to: 'errors#unauthorized'
-  get '404', to: 'errors#not_found'
-  get '418', to: 'errors#im_a_teapot'
-  get '422', to: 'errors#unprocessable_entity'
-  get '451', to: 'errors#unavailable_for_legal_reasons'
-  get '500', to: 'errors#internal_server_error'
-
   get 'sitemap', to: 'sitemap#index'
 
   get "login", to: "login#new", as: :login

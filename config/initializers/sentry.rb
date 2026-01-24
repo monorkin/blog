16bbea22e8d7eb3b563ac2e71925e8842a6cc64f
Stanko K.R. <stanko@stanko.io>
2026-01-24 09:58:53 +0100

    Move from encryted credentials to ENV

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 Sentry.init do |config|
-  config.dsn = ENV['SENTRY_DSN']
+  config.dsn = Rails.app.creds.option(:sentry, :dsn)
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for tracing.
   # We recommend adjusting this value in production.
-  config.traces_sample_rate = ENV.fetch('SENTRY_TRACES_SAMPLE_RATE', 0.1).to_f
+  config.traces_sample_rate = Rails.app.creds.option(:sentry, :traces_sample_rate, default: 0.1)
 
   # Set profiles_sample_rate to profile 100%
   # of sampled transactions.
   # We recommend adjusting this value in production.
-  config.profiles_sample_rate = ENV.fetch('SENTRY_PROFILES_SAMPLE_RATE', 0.1).to_f
+  config.profiles_sample_rate = Rails.app.creds.option(:sentry, :profiles_sample_rate, default: 0.1)
 
   # Filter sensitive data
   config.send_default_pii = false

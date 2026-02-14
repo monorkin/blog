462223a9bfa360894df4cde30a432867f1c13ee4
Stanko K.R. <stanko@stanko.io>
2026-02-14 15:22:05 +0100

    Add YAML support

--- app/javascript/controllers/rich_text_controller.js
+++ app/javascript/controllers/rich_text_controller.js
@@ -16,5 +16,21 @@ export default class extends ApplicationController {
   editorReady() {
     this.element.classList.add(this.readyClass)
     this.element.classList.remove(this.loadingClass)
+    this.#addYamlLanguageOption()
+  }
+
+  // Private
+
+  #addYamlLanguageOption() {
+    for (const select of this.element.querySelectorAll(".lexxy-code-language-picker")) {
+      if (select.querySelector('option[value="yaml"]')) continue
+
+      const option = document.createElement("option")
+      option.value = "yaml"
+      option.textContent = "YAML"
+
+      const insertBefore = Array.from(select.options).find(o => o.textContent.localeCompare("YAML") > 0)
+      select.insertBefore(option, insertBefore || null)
+    }
   }
 }

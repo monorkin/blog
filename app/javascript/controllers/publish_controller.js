f37bdc7abe6d25f08e927f7c3b0eb700e7e64f64
Stanko K.R. <stanko@stanko.io>
2026-02-14 09:56:42 +0100

    Simplify publishing

---
+++ app/javascript/controllers/publish_controller.js
@@ -0,0 +1,34 @@
+import ApplicationController from "controllers/application_controller"
+
+export default class extends ApplicationController {
+  static targets = ["publishedInput", "publishAtInput", "scheduleDatetime", "schedulePopup", "draftControls", "publishedControls"]
+  static values = { published: Boolean }
+
+  publishedValueChanged() {
+    if (this.publishedValue) {
+      this.draftControlsTarget.hidden = true
+      this.publishedControlsTarget.hidden = false
+    } else {
+      this.draftControlsTarget.hidden = false
+      this.publishedControlsTarget.hidden = true
+    }
+  }
+
+  publish() {
+    this.publishedInputTarget.value = "true"
+    this.publishAtInputTarget.value = ""
+  }
+
+  unpublish() {
+    this.publishedInputTarget.value = "false"
+  }
+
+  toggleSchedule() {
+    this.schedulePopupTarget.hidden = !this.schedulePopupTarget.hidden
+  }
+
+  publishAtTime() {
+    this.publishAtInputTarget.value = this.scheduleDatetimeTarget.value
+    this.publishedInputTarget.value = "true"
+  }
+}
